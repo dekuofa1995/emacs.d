ffccacc004721375f6058860edfc739682028467
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2023-05-31 10:36:44 +0800

    refacctor: move common constants to init-const.el

--- lisp/init-const.el
+++ lisp/init-const.el
@@ -20,6 +20,27 @@
 - private: bind and other private configurations."
   )
 
+;; Constants from centaur
+(defconst sys/macp
+  (eq system-type 'darwin)
+  "Are we running on a Mac system?")
+
+(defconst sys/mac-x-p
+  (and (display-graphic-p) sys/macp)
+  "Are we running under X on a Mac system?")
+
+(defconst sys/win32p
+  (eq system-type 'windows-nt)
+  "Are we running on a WinTel system?")
+
+(defconst sys/linuxp
+  (eq system-type 'gnu/linux)
+  "Are we running on a GNU/Linux system?")
+(defconst sys/linux-x-p
+  (and (display-graphic-p) sys/linuxp)
+  "Are we running under X on a GNU/Linux system?")
+
+
 (defvar my-show-icon t)
 
 (provide 'init-const)

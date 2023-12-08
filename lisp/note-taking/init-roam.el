eb18e981aa5425128d210a460baf9ae1c3fb9139
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2023-12-08 21:29:03 +0800

    format code

--- lisp/note-taking/init-roam.el
+++ lisp/note-taking/init-roam.el
@@ -44,33 +44,33 @@
 				(error ""))))
 	(:after transient
 		(transient-define-prefix transient-map-roam ()
-  "Org Roam."
-  [["CAPTURE"
-    ("c" "capture"  org-roam-capture)
-    ("tc" "today cap"  org-roam-dailies-capture-today)
-		"EDIT"
-		("i" "insert" org-roam-node-insert
-		 "rf" "refile"   org-roam-refile
-		 "b" "buffer" org-roam-buffer-display-dedicated
-		 "aa" "add alias" org-roam-alias-add
-		 "ra" "remove alias" org-roam-alias-remove)]
-   ["SHOW"
-    "🢆 Find"
-		("fr" "find ref" org-roam-ref-find)
-		("fn" "find node" org-roam-node-find)
-		("on" "open node" org-roam-node-open)
-		("gv" "grep visit" org-roam-grep-visit)
-		("nv" "node visit" org-roam-node-visit)
-		("gh" "graph" org-roam-graph)
-    "🢆 TIMED"
-    ("gt" "goto today" org-roam-dailies-goto-today)
-    ("gn" "goto next" org-roam-dailies-goto-previous-note)
-    ("gp" "goto prev" org-roam-dailies-goto-previous-note)]
-   ["DB"
-    ("s" "sync"    org-roam-db-sync)
-		;; ("S" "setup" org-roam-db-autosync-enable)
-		("DA" "diagnose" org-roam-diagnostics)
-		("Dn" "diagnose" org-roam-db-diagnose-node)]])
+			"Org Roam."
+			[["CAPTURE"
+				("c" "capture"  org-roam-capture)
+				("tc" "today cap"  org-roam-dailies-capture-today)
+				"EDIT"
+				("i" "insert" org-roam-node-insert
+				 "rf" "refile"   org-roam-refile
+				 "b" "buffer" org-roam-buffer-display-dedicated
+				 "aa" "add alias" org-roam-alias-add
+				 "ra" "remove alias" org-roam-alias-remove)]
+			 ["SHOW"
+				"🢆 Find"
+				("fr" "find ref" org-roam-ref-find)
+				("fn" "find node" org-roam-node-find)
+				("on" "open node" org-roam-node-open)
+				("gv" "grep visit" org-roam-grep-visit)
+				("nv" "node visit" org-roam-node-visit)
+				("gh" "graph" org-roam-graph)
+				"🢆 TIMED"
+				("gt" "goto today" org-roam-dailies-goto-today)
+				("gn" "goto next" org-roam-dailies-goto-previous-note)
+				("gp" "goto prev" org-roam-dailies-goto-previous-note)]
+			 ["DB"
+				("s" "sync"    org-roam-db-sync)
+				;; ("S" "setup" org-roam-db-autosync-enable)
+				("DA" "diagnose" org-roam-diagnostics)
+				("Dn" "diagnose" org-roam-db-diagnose-node)]])
 		(:global
 		 [f3] transient-map-roam))
 	(:with-map org-roam-mode-map

91000d8cf031251b616df0f128a37bcbbf295b1f
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2024-04-18 06:53:42 +0800

    Clean org config code. - init-latex.el: use setup replace 'with-eval-after-load' func -	init-org-style.el: use setup's also-load to load org-modern when load org-mode

--- lisp/ui/init-org-style.el
+++ lisp/ui/init-org-style.el
@@ -2,23 +2,23 @@
 ;;; Commentary:
 
 (setup org-modern
-	(:load+ org-modern)
   (:hooks org-mode-hook org-modern-mode)
   (:hooks org-agenda-finalize-hook org-modern-agenda))
 
 (setup org
 	(:doc "customize org style")
+	(:also-load org-modern)
 	(:option
 	 org-todo-keyword-faces '(("TODO"    . warning)
-                               ("DOING"   . success)
-                               ("WAITING" . error)
-                               ("VERIFY"  . error)
-                               ("DONE"    . shadow)
-                               ("CANCEL"  . shadow))
+                            ("DOING"   . success)
+                            ("WAITING" . error)
+                            ("VERIFY"  . error)
+                            ("DONE"    . shadow)
+                            ("CANCEL"  . shadow))
 	 org-hide-emphasis-markers t)
 	(:when-loaded
 		(add-to-list 'org-emphasis-alist
-							 '("=" (:box (:line-width -2 :color "gray50" :style released-button) :inherit org-verbatim)))))
+								 '("=" (:box (:line-width -2 :color "gray50" :style released-button) :inherit org-verbatim)))))
 
 (setup org-sticky-header
   (:hooks org-mode-hook org-sticky-header-mode))

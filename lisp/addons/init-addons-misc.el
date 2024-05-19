52e9db83b0b8c6b1f70c3a33e61ebf232f670e6a
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2024-05-19 15:58:06 +0800

    Setup mu4e and epa-file for email

--- lisp/addons/init-addons-misc.el
+++ lisp/addons/init-addons-misc.el
@@ -16,7 +16,6 @@
 (setup diff-hl
   (:hooks (list prog-mode-hook conf-mode-hook) diff-hl-mode))
 
-
 (setup vterm
 	(defun +setup-vterm-font ()
 		(set (make-local-variable 'buffer-face-mode-face) `(:family ,deku/term-font))
@@ -117,5 +116,90 @@
 	(:init
 	 (add-to-list 'auto-mode-alist
 								'("\\.http\\'" . restclient-mode))))
+
+
+(setup mu4e
+	(:load+ mu4e)
+	(:also-load smtpmail)
+	(:also-load epa-file)
+	(:autoload mu4e)
+	(:option*
+	 mu4e-maildir "~/mail/"
+	 mu4e-get-mail-command (concat (executable-find "mbsync") " -a")
+	 mu4e-update-interval 300 ;; in seconds
+	 mu4e-attachment-dir "~/mail/attachment/"
+	 mu4e-change-filenames-when-moving t
+	 mu4e-user-mailing-lists '("mob5277@outlook.com")
+	 mu4e-maildir-shortcuts '(("/outlook/INBOX" . ?o)
+														("/outlook/Sent" . ?O)
+														("/deku/INBOX" . ?d)
+														("/deku/Sent" . ?D))
+	 mu4e-context-policy 'pick-first
+	 mu4e-compose-context-policy 'ask
+	 mu4e-contexts
+	 `(,(make-mu4e-context
+			 :name "outlook"
+			 :enter-func
+			 (lambda () (mu4e-message "Enter mob5277@outlook.com context"))
+			 :leave-func
+			 (lambda () (mu4e-message "Leave mob5277@outlook.com context"))
+			 :match-func
+			 (lambda (msg)
+				 (when msg
+					 (mu4e-message-contact-field-matches msg :to "mob5277@outlook.com")))
+			 :vars '((user-mail-address . "mob5277@outlook.com")
+							 (user-full-name . "Mob")
+							 (mu4e-drafts-folder . "/Drafts")
+							 (mu4e-refile-folder . "/Archive")
+							 (mu4e-sent-folder . "/Sent")
+							 (mu4e-trash-folder . "/Deleted")))
+		 ,(make-mu4e-context
+			 :name "deku"
+			 :enter-func
+			 (lambda () (mu4e-message "Enter dekuofa1995@gmail.com context"))
+			 :leave-func
+			 (lambda () (mu4e-message "Leave dekuofa1995@gmail.com context"))
+			 :match-func
+			 (lambda (msg)
+				 (when msg
+					 (mu4e-message-contact-field-matches msg :to "dekuofa1995@gmail.com")))
+			 :vars '((user-mail-address . "dekuofa1995@gmail.com")
+							 (user-full-name . "dekuofa1995")
+							 (mu4e-drafts-folder . "/Drafts")
+							 (mu4e-refile-folder . "/Archive")
+							 (mu4e-sent-folder . "/Sent")
+							 (mu4e-trash-folder . "/Deleted"))))
+	 )
+	;; (add-to-list 'mu4e-bookmarks
+	;; 						 (mu4e-bookmark-define "mail:/outlook/INBOX" "Inbox - outlook" ?o))
+	)
+
+(defun deku/set-msmtp-account ()
+  (if (message-mail-p)
+      (save-excursion
+        (let*
+            ((from (save-restriction
+                     (message-narrow-to-headers)
+                     (message-fetch-field "from")))
+             (account
+              (cond
+               ((string-match "mob5277@outlook.com" from) "outlook")
+               ((string-match "dekuofa1995@gmail.com" from) "deku")
+               ;; ((string-match "dummy@example.com" from) "example")
+							 )))
+          (setq message-sendmail-extra-arguments (list '"-a" account))))))
+
+(setup epa-file
+	(:when-loaded
+		(epa-mail-mode)
+		(auth-source-forget-all-cached)
+		(add-hook 'message-send-mail-hook 'deku/set-msmtp-account))
+	(:option*
+	 epg-pinentry-mode 'loopback
+	 message-kill-buffer-on-exit t
+	 send-mail-function 'sendmail-send-it
+	 message-send-mail-function 'sendmail-send-it
+	 sendmail-program (executable-find "msmtp")
+	 message-sendmail-envelope-from 'header))
 (provide 'init-addons-misc)
 ;;; init-addons-misc.el ends here

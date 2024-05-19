91252076a87f3afec4e630c9ed7c6a6c11b7a158
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2024-05-19 16:10:22 +0800

    Add tangle for typst code from https://emacs-china.org/t/tangle-typst-typst/27226

--- lisp/langs/init-typst.el
+++ lisp/langs/init-typst.el
@@ -16,34 +16,6 @@
 	 typst-ts-mode-enable-raw-blocks-highlight t
 	 typst-ts-mode-highlight-raw-blocks-at-startup t))
 
-;; (defvar tip--server-process
-;; 	nil
-;; 	"Tip server process's object.")
-
-;; (defun tip--start-server (buf port)
-;; 	(setq tip-server-process
-;; 				(start-file-process-shell-command
-;; 				 "tip-server"
-;; 				 buf
-;; 				 (format "bin/python main.py --port=%s" port))))
-
-;; (defun tip-ensure (&optional force)
-;; 	(interactive)
-;; 	(let ((port tip-server-port)
-;; 				(dir (file-truename tip-server-basedir))
-;; 				(process tip--server-process)
-;; 				(buf (get-buffer-create "*tip-server-log*")))
-;; 		(with-current-buffer buf
-;; 			(setq-local default-directory dir)
-;; 			(cond ((and process force)
-;; 						 (message "kill and start a new tip-server.")
-;; 						 (kill-process process)
-;; 						 (tip--start-server buf port))
-;; 						((and process (not force))
-;; 						 (message "tip-server alreadly started!"))
-;; 						(t
-;; 						 (tip--start-server buf port))))))
-
 (setup tip
 	(:once '(:hooks typst-ts-mode-hook)
 		(require 'tip))
@@ -53,5 +25,47 @@
 	 tip-server-port 8111
 	 tip-server-basedir (file-truename "~/git/tools/tip-server-py/")))
 
+;; tangle in typst from nasy https://emacs-china.org/t/tangle-typst-typst/27226
+(defvar typst--initialized-files nil
+  "List of files that have been initialized for the current tangle operation.")
+
+(defun typst--initialize-file (path)
+  "Initialize a temporary file for the PATH and return the temp file's path."
+  (let ((temp-path (make-temp-file "typst" nil ".tmp")))
+    (push (cons temp-path path) typst--initialized-files)
+    temp-path))
+
+(defun typst--tangle (node)
+  "Recursively process each raw_blck NODE in the treesit parsed tree."
+  (if (equal (treesit-node-type node) "raw_blck")
+      (let* ((beg (treesit-node-start node))
+             (path-line (treesit-node-text (treesit-node-on (- beg 2) (- beg 1))))
+             (original-path (when (> (length path-line) 2) (string-trim (substring path-line 2))))
+             (temp-path (when original-path
+                          (or (car (rassoc original-path typst--initialized-files))
+                              (typst--initialize-file original-path))))
+             (content-node (car (treesit-filter-child node (lambda (n) (equal (treesit-node-type n) "blob")))))
+             (content (when content-node (string-trim (treesit-node-text content-node)))))
+        (when temp-path
+          (append-to-file content nil temp-path)
+          (append-to-file "\n" nil temp-path)
+          (message "Tangled to temp file: %s." temp-path)))
+    (dolist (child (treesit-node-children node))
+      (typst--tangle child))))
+
+
+(defun typst-tangle ()
+  "Tangle the current typst file."
+  (interactive)
+  (setq typst--initialized-files nil)
+  (let ((root (treesit-buffer-root-node)))
+    (typst--tangle root)
+    (dolist (pair typst--initialized-files)
+      (let ((temp-path (car pair))
+            (original-path (cdr pair)))
+        (rename-file temp-path original-path t)
+        (message "Moved %s to %s" temp-path original-path)))
+    (setq typst--initialized-files nil)))
+;; tangle end
 (provide 'init-typst)
 ;;; init-typst.el ends here

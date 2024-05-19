affa94c0ba06162f810e4acbf4ff66e17d21ba42
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2024-05-19 15:57:19 +0800

    Update dape for java debug

--- lisp/langs/init-java.el
+++ lisp/langs/init-java.el
@@ -15,13 +15,19 @@
                (data-dir (expand-file-name (file-name-concat jdtls-cache-dir (md5 project-dir))))
                (jvm-args `("-Xmx8G"
 													 "-XX:+UseZGC"
+													 "-Xint"
 													 "-XX:+UseStringDeduplication"
 													 ;; "-XX:FreqInlineSize=325"
 													 ;; "-XX:MaxInlineLevel=9"
 													 "-XX:+UseCompressedOops"))
                (jvm-args (mapcar (lambda (arg) (concat "--jvm-arg=" arg)) jvm-args))
                ;; tell jdtls the data directory and jvm args
-               (contact (append '("jdtls") jvm-args `("-data" ,data-dir))))
+               (contact (append '("jdtls")
+																jvm-args
+																`("-data" ,data-dir)
+																`(:initializationOptions
+																	(:bundles
+																	 [,(file-truename "~/git/tools/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.52.0.jar")])))))
 					contact))
       (push '((java-mode java-ts-mode) . jdtls-command-contact) eglot-server-programs))))
 

f931dbd28cf70f3517e21beb44b75f508b438912
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2025-02-10 14:52:28 +0800

    update: config for aider

--- lisp/addons/init-ai.el
+++ lisp/addons/init-ai.el
@@ -5,6 +5,10 @@
 (setup aider
 	(:url "https://github.com/tninja/aider.el?tab=readme-ov-file")
 	(:doc "aider.el : aider (AI Pair Programming) Inside Emacs")
+	(:option*
+	 aider-args '("--model" "deepseek"))
+	(:when-loaded
+		(setenv "DEEPSEEK_API_KEY" deku/deepseek-api-key))
 	(:global
 	 "C-c a" aider-transient-menu))
 

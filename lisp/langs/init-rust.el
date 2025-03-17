0a29e06c32da5b0acf4454878a7e184a757c9837
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2025-03-17 09:25:28 +0800

    setup: rust-mode, rust-playgroud and cargo-mode

--- lisp/langs/init-rust.el
+++ lisp/langs/init-rust.el
@@ -2,10 +2,17 @@
 ;;; Commentary:
 
 (setup rust-mode
+	(:also-load rust-playgroud)
   (:hooks
-   rust-mode-hook eglot-ensure)
-  (:when-loaded
-    (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))))
+   (rust-ts-mode-hook rust-mode-hook) eglot-ensure)
+	(:file-match (rx ".rs$")))
+
+(setup cargo-mode
+	(:option*
+	 compilation-scroll-output t)
+	(:hooks (rust-mode-hook rust-ts-mode-hook) cargo-minor-mode))
+
+(setup rust-playgroud)
 
 (provide 'init-rust)
 ;;; init-rust.el ends here

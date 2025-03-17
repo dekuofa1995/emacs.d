822e070a050dec5bd07dabc90417c0a206d860ca
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2025-03-17 09:26:58 +0800

    fix: wrong style in org-modern-todo-faces

--- lisp/ui/init-org-style.el
+++ lisp/ui/init-org-style.el
@@ -5,8 +5,8 @@
 	(:option* org-modern-todo-faces
 						(let ((weight 'bold)
 									(color "white"))
-							`(("TODO" :background "#cc9393" :foreground ,color :weight ,weight :height "inherit")
-								("DOING" :background "#7cb8bb" :foreground ,color :weight ,weight :height "inherit")
+							`(("TODO" :background "#cc9393" :foreground ,color :weight ,weight)
+								("DOING" :background "#7cb8bb" :foreground ,color :weight ,weight)
 								("WAITING" :background "#d0bf8f" :foreground ,color :weight ,weight)
 								("CANCEL" :background "#8c5353" :foreground ,color :weight ,weight)
 								("DONE" :background "#afd8af" :foreground ,color :weight ,weight))))
